8f98170290f9587fae62735477d33ca93ab352d8
Juliana Cuani <juliana.271@hotmail.com>
2020-10-31 15:23:14 -0300

    versao1 do app ok

--- app/src/main/java/com/example/whatdidilearn/view/MainActivity.kt
+++ app/src/main/java/com/example/whatdidilearn/view/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.whatdidilearn.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.whatdidilearn.R
+import com.example.whatdidilearn.data.DataBaseItens
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recycleView = findViewById<RecyclerView>(R.id.items_recycle_view)
+        val adapter = LearnedItemsAdapter()
+        recycleView.adapter = adapter
+
+        val learnedItemsList = DataBaseItens().getAll()
+        adapter.data = learnedItemsList
     }
 }
