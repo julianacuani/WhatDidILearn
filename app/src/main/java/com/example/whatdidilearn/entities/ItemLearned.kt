0bddd8b8521f67760a41b72dcb1fb48adaee12a8
Juliana Cuani <juliana.271@hotmail.com>
2020-10-31 11:19:11 -0300

    criando niveis e classe learn

---
+++ app/src/main/java/com/example/whatdidilearn/entities/ItemLearned.kt
@@ -0,0 +1,9 @@
+package com.example.whatdidilearn.entities
+
+import android.icu.text.CaseMap
+
+class ItemLearned (
+    val title: String,
+    val description: String,
+    val understandingLevel: Level
+)
