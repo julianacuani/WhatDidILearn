c243ea8fec3b3de7ecd5cdc3eee28d4450c9e629
Juliana Cuani <juliana.271@hotmail.com>
2020-11-03 20:56:58 -0300

    criada a entidade do room - bd

--- app/src/main/java/com/example/whatdidilearn/entities/ItemLearned.kt
+++ app/src/main/java/com/example/whatdidilearn/entities/ItemLearned.kt
@@ -1,9 +1,22 @@
 package com.example.whatdidilearn.entities
 
 import android.icu.text.CaseMap
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-class ItemLearned (
+@Entity(tableName = "learned_item")
+data class ItemLearned (
+    @ColumnInfo(name = "item_title")
     val title: String,
+
+    @ColumnInfo(name = "item_description")
     val description: String,
-    val Level: Level
+
+    @ColumnInfo(name = "item_level")
+    val Level: Level,
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "item_id")
+    val id: Int = 0
 )
